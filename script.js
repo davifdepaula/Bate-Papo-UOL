37c0c0d9821f45b4523a186c6de566df2f653902
davifdepaula <davi.ferreira100@gmail.com>
2022-10-30 21:15:49 -0300

    falta o scroll

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
-let user = "Davi"
-function creatUser() {
-    const participants = {
-        name: user
-    }
-    return participants
-}
-
-function getTime(){
-    const time = new Date()
-    const h = time.getHours()
-    const m = time.getMinutes()
-    const s = time.getSeconds()
-    return `${h}:${m}:${s}`
-}
-
-// comeIn()
+let user = "Davi", msgList = []
 
 function comeIn() {
     const url = "https://mock-api.driven.com.br/api/v6/uol/participants"
-    const participants = creatUser()
-    const response = axios.post(url, {name: "Davi"})
-    response.then((result) => {
-        console.log("first", result)
-        if (result.status === 200){
-            const content = document.querySelector("main .users")
-            content.innerHTML += `<div class="user inOut"> 
-            <span class = "time">(${getTime()})</span> 
-            <span class="userName">${participants.name}</span> 
-            entrou na sala...</div>`
-        }
-    }).catch((err) => {
+    axios.post(url, {name: user})
+    .then((response) => console.log("Usuario entrou: ", response.status))
+    .catch((err) => {
         if (err.response.status === 400) {
-            user = "Lari"
-            // user = prompt(`Esse nome já existe. Escolha outro para usar o bate papo`)
-            // creatUser()
-            // comeIn()
+            user = prompt(`Esse nome já existe. Escolha outro para usar o bate papo`)
+            comeIn()
         }
     });
 }
@@ -45,16 +18,76 @@ const users = []
 function active(){
     const url = "https://mock-api.driven.com.br/api/v6/uol/status"
     axios.post(url, {name: user})
-        .then((response) => console.log("ainda to aqui"))
-        .catch((error) => {
-            const content = document.querySelector("main .users")
-            content.innerHTML += `<div class="user inOut"> 
-            <span class = "time">(${getTime()})</span> 
-            <span class="userName">${participants.name}</span> 
-            entrou na sala...</div>`
-
-        })
+        .then((response) => console.log("status", response.status))
+        .catch((error) => console.log(error))
 }
 
 
 setInterval(active, 5000)
+
+function getMsg(){
+    const url = "https://mock-api.driven.com.br/api/v6/uol/messages"
+    axios.get(url)
+        .then((response) => {  
+            msgList = response.data
+            msgList.map((msg) => {
+                return renderMsg(msg)
+            })     
+        })
+        .catch((error) => console.log("msg não veio", error))
+}
+getMsg()
+
+function renderMsg(msg) {
+    const content =  document.querySelector("main")
+    if (msg.type === "status"){
+        content.innerHTML += `<div class= "inOut share">
+        <span class="time">(${msg.time})</span>$ 
+        <span class="userName">${msg.from}</span> ${msg.text}
+        </div>
+        `
+    }
+    else if(msg.type === "message"){
+        content.innerHTML += `<div class= "public share">
+        <span class="time">(${msg.time})</span>$ 
+        <span class="userName">${msg.from}</span> para 
+        <span class="userName">${msg.to}</span>
+        ${msg.text}
+        </div>
+        `
+    }
+
+    else if(msg.type === "private_message"){
+        content.innerHTML += `<div class= "private share">
+        <span class="time">(${msg.time})</span>$ 
+        <span class="userName">${msg.from}</span> para 
+        <span class="userName">${msg.to}</span>
+        ${msg.text}
+        </div>
+        `
+    }
+}
+
+function creatMsg() {
+    const content = document.querySelector(".msgContent")
+    const msg =  content.value
+    return {
+        from: user, 
+        to: "todos",
+        text: msg,
+        type: "message"
+    }
+}
+
+
+function sendMsg(){
+    const msgContent = creatMsg()
+    const url = "https://mock-api.driven.com.br/api/v6/uol/messages"
+    axios.post(url, msgContent)
+        .then((response) => {
+            const show = document.querySelector("main")
+            show.scrollIntoView()
+            console.log("foi")
+    })
+        .catch((error) => window.location.reload())
+}
